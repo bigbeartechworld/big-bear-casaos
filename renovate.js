38056332148d57d8d682a26324ff13153bee0667
Christopher <github.bigbeartechworld@emailcove.com>
2023-11-05 14:00:46 -0600

    Add Renovate GitHub Actions workflow and configuration file

---
+++ renovate.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ["config:base"], // Extending a base configuration named "config:base"
+  onboarding: false, // Disabling onboarding process
+  requireConfig: false, // Not requiring a specific configuration
+  rebaseWhen: "conflicted", // Rebase when there are conflicts
+  gitIgnoredAuthors: ["githubaction@githubaction.com"], // Ignoring commits from a specific author
+  dependencyDashboard: true, // Enabling the dependency dashboard
+  enabledManagers: ["docker-compose", "dockerfile"], // Enabling specific package managers
+
+  hostRules: [
+    {
+      matchHost: "index.docker.io", // Matching a specific host
+      hostType: "docker", // Defining the host type as "docker"
+      username: process.env.DOCKERHUB_USERNAME, // Setting the username from environment variables
+      password: process.env.DOCKERHUB_TOKEN, // Setting the password from environment variables
+    },
+    {
+      matchHost: "docker.io", // Matching another specific host
+      concurrentRequestLimit: 2, // Limiting concurrent requests to 2 for this host
+    },
+  ],
+
+  packageRules: [
+    {
+      managers: ["docker-compose", "dockerfile"], // Applying rules to specific package managers
+      packagePatterns: [
+        "^([^\\/]+\\/)?(mysql|mariadb|mongodb|mongo|postgres|redis)(:|$)",
+      ], // Defining package patterns using regular expressions
+      enabled: false, // Disabling this rule
+    },
+  ],
+};
