05cd53de5a66a7800fe5d1a8218a10efa14982b7
Christopher <github.bigbeartechworld@emailcove.com>
2023-12-13 18:45:17 -0600

    Update renovate.js configuration

--- renovate.js
+++ renovate.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["config:recommended"], // Extending a base configuration named "config:base"
-  onboarding: false, // Disabling onboarding process
+  extends: ["config:recommended", ":dependencyDashboard"], // Extending a base configuration named "config:base"
+  onboarding: true, // Disabling onboarding process
   rebaseWhen: "conflicted", // Rebase when there are conflicts
   gitIgnoredAuthors: ["githubaction@githubaction.com"], // Ignoring commits from a specific author
   dependencyDashboard: true, // Enabling the dependency dashboard
